27cc59947071a765805602730d5370b0fdb8288a
brainvlad <wladiik@gmail.com>
2021-12-26 20:32:49 +0300

    fix

--- Ретро (слайдер)/js/preloader.js
+++ Ретро (слайдер)/js/preloader.js
@@ -1,8 +1,11 @@
 
 window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-    }, 500);
+    setInterval(() => {
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function () {
+            document.body.classList.add('loaded');
+            document.body.classList.remove('loaded_hiding');
+        }, 500);
+    }, 2000);
+
 }
